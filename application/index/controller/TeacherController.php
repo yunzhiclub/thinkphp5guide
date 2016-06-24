73f774679f39c2d06f880eea97f328c199018d0f
mskies <3792535@qq.com>
2016-06-24 10:12:49 +0800

    3.3.1.4

--- application/index/controller/TeacherController.php
+++ application/index/controller/TeacherController.php
@@ -30,14 +30,13 @@ class TeacherController extends Controller
         $teacher['username'] = 'wangwu';
         $teacher['sex'] = '1';
         $teacher['email'] = 'wangwu@yunzhi.club';
-        var_dump($teacher);
 
         // 引用teacher数据表对应的模型
         $Teacher = new Teacher();
-        var_dump($Teacher);
 
         // 向teacher表中插入数据并判断是否插入成功
-        $state = $Teacher->data($teacher)->save();
-        var_dump($state);
+        $Teacher->data($teacher)->save();
+
+        return $teacher['name'] . '成功增加至数据表';
     }
 }
