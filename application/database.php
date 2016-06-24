428bdd48a88cd748daa99f940bb9a3fe77c86bfa
mskies <3792535@qq.com>
2016-06-24 09:36:22 +0800

    3.2.2

--- application/database.php
+++ application/database.php
@@ -18,7 +18,7 @@ return [
     // 服务器地址
     'hostname'    => '127.0.0.1',
     // 数据库名
-    'database'    => '',
+    'database'    => 'tp5',
     // 数据库用户名
     'username'    => 'root',
     // 数据库密码
@@ -30,7 +30,7 @@ return [
     // 数据库编码默认采用utf8
     'charset'     => 'utf8',
     // 数据库表前缀
-    'prefix'      => '',
+    'prefix'      => 'yunzhi_',
     // 数据库调试模式
     'debug'       => APP_DEBUG,
     // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
