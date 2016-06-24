84aba43749ea0536b9861f9c0ce78865790cc6c1
mskies <3792535@qq.com>
2016-06-24 09:57:39 +0800

    3.2.5.1

--- application/index/controller/TeacherController.php
+++ application/index/controller/TeacherController.php
@@ -1,25 +1,24 @@
 <?php
 namespace app\index\controller;
+use think\Controller;   // 用于与V层进行数据传递
 use app\model\Teacher;  // 教师模型
 /**
- * 教师管理
+ * 教师管理, 继承think\Controller后，就可以利用V层对数据进行打包了。
  */
-class TeacherController
+class TeacherController extends Controller
 {
     public function index()
     {
-        // $Teacher 首写字大写，说明它是一个对象, 更确切一些说明这是基于Teacher这个模型被我们手工实例化的，如果存在teacher数据表的话，它对应teacher数据表
         $Teacher = new Teacher; 
-
-        // $teachers 以s结尾，表示它是一个数组，数据中的每一项都是一个对象，这个对象基于Teahcer这个模型。
         $teachers = $Teacher->select();
 
-        // 获取第0个数据
-        $teacher = $teachers[0];
+        // 向V层传数据
+        $this->assign('hello', $teachers);
 
-        // 调用上述对象的getData()方法
-        var_dump($teacher->getData('name'));
-        echo $teacher->getData('name');
-        return $teacher->getData('name');
+        // 取回打包后的数据
+        $htmls = $this->fetch();
+
+        // 将数据返回给用户
+        return $htmls;
     }
 }
---
+++ runtime/temp/4c55891335fa2bc16b48bdbaef315992.php
@@ -0,0 +1,26 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:104:"/Applications/XAMPP/xamppfiles/htdocs/thinkphp5guide/public/../application/index/view/teacher/index.html";i:1466733405;}*/ ?>
+<!DOCTYPE html>
+<html lang="zh-cn">
+<head>
+    <meta charset="UTF-8">
+    <title>教师管理</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>序号</th>
+            <th>姓名</th>
+            <th>性别</th>
+            <th>邮箱</th>
+            <th>用户名</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>张三</td>
+            <td>男</td>
+            <td>11@maol.com</td>
+            <td>zhangsan</td>
+        </tr>
+    </table>
+</body>
+</html>
