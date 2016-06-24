c3c936ea9f12557579f085b9a77b225d38c759f8
mskies <3792535@qq.com>
2016-06-24 09:26:23 +0800

    2.5.1

---
+++ application/test/controller/Index.php
@@ -0,0 +1,10 @@
+<?php
+namespace app\test\controller;
+
+class Index
+{
+    public function index()
+    {
+         return 'Hello yunzhi';
+    }
+}
---
+++ application/yunzhi/controller/Yunzhier.php
@@ -0,0 +1,10 @@
+<?php
+namespace app\yunzhi\controller;
+
+class Yunzhier
+{
+    public function index()
+    {
+         return 'hi yunzhier';
+    }
+}
