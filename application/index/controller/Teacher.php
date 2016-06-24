8964d518bdf5380f6f6f0a1c0e5707027ec6c70e
mskies <3792535@qq.com>
2016-06-24 09:46:41 +0800

    3.2.4.1

--- application/index/controller/Teacher.php
+++ application/index/controller/Teacher.php
@@ -1,18 +1,14 @@
 <?php
 namespace app\index\controller;
-use think\Db;       // 数据库操作类
-
+use app\model\Teacher;  // 教师模型
+/**
+ * 教师管理
+ */
 class Teacher
 {
     public function index()
     {
-        // 获取教师表中的所有数据
-        $teachers = DB::name('teacher')->select();
-
-        // 用下面的语句，也可以直接返回给用户
-        echo $teachers[0]['name'];
-        
-        // 查看获取的数据
-        return $teachers[0]['name'];
+        $Teacher = new Teacher;
+        dump($Teacher);
     }
 }
---
+++ application/model/model/Teacher.php
@@ -0,0 +1,10 @@
+<?php
+namespace app\model;
+use think\Model;    // 使用前进行声明
+/**
+ * Teacher 教师表
+ */
+class Teacher extends Model
+{
+
+}
