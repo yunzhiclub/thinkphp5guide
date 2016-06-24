838e7b1b520c29590fca07f37d15b67ee948de4c
mskies <3792535@qq.com>
2016-06-24 09:54:46 +0800

    3.2.4.5

--- application/index/controller/TeacherController.php
+++ application/index/controller/TeacherController.php
@@ -8,8 +8,18 @@ class TeacherController
 {
     public function index()
     {
-        $Teacher = new Teacher;
+        // $Teacher 首写字大写，说明它是一个对象, 更确切一些说明这是基于Teacher这个模型被我们手工实例化的，如果存在teacher数据表的话，它对应teacher数据表
+        $Teacher = new Teacher; 
+
+        // $teachers 以s结尾，表示它是一个数组，数据中的每一项都是一个对象，这个对象基于Teahcer这个模型。
         $teachers = $Teacher->select();
-        var_dump($teachers);
+
+        // 获取第0个数据
+        $teacher = $teachers[0];
+
+        // 调用上述对象的getData()方法
+        var_dump($teacher->getData('name'));
+        echo $teacher->getData('name');
+        return $teacher->getData('name');
     }
 }
